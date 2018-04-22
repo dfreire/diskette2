9017bf8c3b0a145dd25f47ee1a7c2bdb48422798
dfreire <dario.freire@gmail.com>
2018-04-22 17:48:23 +0100

    refactoring

--- client-web/src/index.tsx
+++ client-web/src/index.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import 'tailwindcss/dist/tailwind.min.css';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { init } from '@rematch/core';
-import * as models from './models';
+import registerServiceWorker from './registerServiceWorker';
 import App from './App';
+import * as models from './models';
+import 'tailwindcss/dist/tailwind.min.css';
+import './index.css';
 
 const store = init({ models });
 
