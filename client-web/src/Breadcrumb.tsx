9d5cf2450c24bea88f412cfd10e8483da8e09820
dfreire <dario.freire@gmail.com>
2018-04-27 18:37:23 +0100

    fixed Breadcrumb

--- client-web/src/Breadcrumb.tsx
+++ client-web/src/Breadcrumb.tsx
@@ -4,9 +4,12 @@ import { Link } from 'react-router-dom';
 class Breadcrumb extends React.Component {
     render() {
         const tokens = window.location.pathname.split('/').filter(t => t.length > 0);
-        const links = [];
+        const links = [{
+            to: '/content',
+            component: <Link className="text-black no-underline" to={'/content'}>home</Link>,
+        }];
 
-        for (let i = 0; i < tokens.length; i++) {
+        for (let i = 1; i < tokens.length; i++) {
             const previousTo: string = i === 0 ? '' : links[i - 1].to;
             const to = previousTo + '/' + tokens[i];
             const component = <Link className="text-black no-underline" to={to}>{tokens[i]}</Link>;
@@ -16,7 +19,7 @@ class Breadcrumb extends React.Component {
         return (
             <div className="bg-grey-light p-3">
                 {links.map((link, i) => (
-                    <span key={link.to}>{link.component} {i < tokens.length - 1 ? '/' : ''}</span>
+                    <span key={link.to}>{link.component} {i < tokens.length - 1 ? ' / ' : ''}</span>
                 ))}
             </div>
         );
