3e4fa6c119041439e95c2d98481ca2920c879889
dfreire <dario.freire@gmail.com>
2018-04-27 18:28:56 +0100

    navigating content

--- client-web/src/models/Content.ts
+++ client-web/src/models/Content.ts
@@ -3,35 +3,67 @@ import * as UserModel from './User';
 import { logoutIf401 } from './util';
 
 export interface State {
+    contentPage: {
+        content: Content;
+        contentType: ContentType;
+        errorMessage: string;
+    };
+}
+
+interface Content {
+    type: string;
+    fields: object;
+    subDirs: string[];
+}
+
+interface ContentType {
+
 }
 
 export interface Dispatch {
-    onLoad: { (): void };
+    onLoad: { (payload: { pathname: string }): void };
+    onLoaded: { (payload: { content: Content, contentType: ContentType }): void };
 };
 
 const INITIAL_STATE: State = {
+    contentPage: {
+        content: {
+            type: '',
+            fields: {},
+            subDirs: [],
+        },
+        contentType: {
+        },
+        errorMessage: '',
+    }
 };
 
 const reducers = {
+    onLoaded(state: State, payload: { content: Content, contentType: ContentType }) {
+        const { content, contentType } = payload;
+        const contentPage = { ...INITIAL_STATE.contentPage, content, contentType };
+        return { ...state, contentPage };
+    },
 };
 
 const effects = {
-    async onLoad(payload: {}, rootState: { content: State, user: UserModel.State }) {
-        console.log('onLoad', rootState);
+    async onLoad(payload: { pathname: string }, rootState: { content: State, user: UserModel.State }) {
+        const { pathname } = payload;
+        console.log('onLoad pathname', pathname);
         try {
-            const res1 = await axios.get('/api/content', {
+            const res1 = await axios.get(`/api${pathname}`, {
                 headers: { Authorization: `Bearer ${rootState.user.sessionToken}` },
             });
 
-            console.log('res1.data', res1.data);
             const content = res1.data;
 
             const res2 = await axios.get(`/api/types/${content.type}`, {
                 headers: { Authorization: `Bearer ${rootState.user.sessionToken}` },
             });
 
-            console.log('res2.data', res2.data);
+            const contentType = res2.data;
 
+            dispath(this).onLoaded({ content, contentType });
         } catch (err) {
             console.error(err);
             logoutIf401(err);
@@ -39,13 +71,12 @@ const effects = {
     },
 };
 
-// function dispath(that: any) {
-//     return (that as any) as Dispatch;
-// }
+function dispath(that: any) {
+    return (that as any) as Dispatch;
+}
 
 export const content = {
     state: { ...INITIAL_STATE },
     reducers,
     effects
 };
-
--- client-web/src/pages/Content.tsx
+++ client-web/src/pages/Content.tsx
@@ -1,26 +1,53 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as ContentModel from '../models/Content';
 
 interface Props extends ContentModel.State, ContentModel.Dispatch {
+    location: Location;
 }
 
-class Content extends React.Component<Props, {}> {
+interface State { };
+
+class Content extends React.Component<Props, State> {
     render() {
         return (
             <div>
                 {console.log('Content', this.props) && false}
                 <h2>Content</h2>
+                {this._renderSubDirs()}
+                <div>{JSON.stringify(this.props.contentPage)}</div>
             </div>
         );
     }
 
+    _renderSubDirs() {
+        return (
+            <ul>
+                {this.props.contentPage.content.subDirs.map(subDir => {
+                    return (
+                        <li key={subDir}>
+                            <Link to={`${this.props.location.pathname}/${subDir}`}>{subDir}</Link>
+                        </li>
+                    );
+                })}
+            </ul>
+        );
+    }
+
     componentDidMount() {
-        this.props.onLoad();
+        this.props.onLoad({ pathname: this.props.location.pathname });
+    }
+
+    componentDidUpdate(prevProps: Props, prevState: State) {
+        if (prevProps.location.pathname != this.props.location.pathname) {
+            this.props.onLoad({ pathname: this.props.location.pathname });
+        }
     }
 }
 
 const mapState = (models: { content: ContentModel.State }) => ({
+    contentPage: models.content.contentPage,
 });
 
 const mapDispatch = (models: { content: ContentModel.Dispatch }) => ({
