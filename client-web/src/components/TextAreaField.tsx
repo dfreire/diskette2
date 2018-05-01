407747d00ef4da0439a844589aef39b9bd8b65d1
dfreire <dario.freire@gmail.com>
2018-05-01 15:03:42 +0100

    web design

--- client-web/src/components/TextAreaField.tsx
+++ client-web/src/components/TextAreaField.tsx
@@ -10,10 +10,10 @@ const TextAreaField = (props: Props) => (
     <div className={classNames.field}>
         <label className={classNames.label}>{props.label}</label>
         <textarea
-            className={classNames.input}
+            className={classNames.textarea}
             value={props.value}
             onChange={evt => props.onChange(evt.target.value)}
-            rows={20}
+            rows={15}
         />
     </div>
 );
@@ -21,7 +21,7 @@ const TextAreaField = (props: Props) => (
 const classNames = {
     field: "w-full py-2",
     label: "block pb-2 text-grey-darkest font-medium",
-    input: "block p-2 w-full border rounded",
+    textarea: "block p-2 w-full border rounded leading-normal",
 }
 
 export default TextAreaField;
--- client-web/src/pages/Content/SubDirs.tsx
+++ client-web/src/pages/Content/SubDirs.tsx
@@ -11,7 +11,7 @@ interface Props extends ContentModel.State, ContentModel.Dispatch {
 const SubDirs = (props: Props) => {
     return (
         <div className="text-sm">
-            <div className="py-3 md:px-3">
+            <div className="p-2 md:px-3">
                 <button className={"w-full bg-green hover:bg-green-light text-white p-3 rounded"}>
                     <Icon name="plus" />
                 </button>
